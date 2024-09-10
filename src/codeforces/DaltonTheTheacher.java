9467c2e43ae7e6f1754791d37b6ec728a76cefb2
David-alzate <alzatedavid0126@gmail.com>
2024-09-10 14:33:55 -0500

    se termino el algoritmo del theacher con los estudiantes felices y infelices

--- src/codeforces/DaltonTheTheacher.java
+++ src/codeforces/DaltonTheTheacher.java
@@ -6,30 +6,30 @@ public class DaltonTheTheacher {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int n = reader.nextInt();
-        int feli = 0;
-        int infeli = 0;
-        int [] array = new int[n];
+        int t = reader.nextInt();
 
-        for (int i = 0; i < n; i++){
-            array[i] = reader.nextInt();
-        }
+        for (int test = 0; test < t; test++) {
+            int n = reader.nextInt();
+            int infeli = 0;
+            int[] array = new int[n];
 
-        for (int i = 0; i < n; i++){
-            if (array[i] == i + 1){
-                infeli = infeli + 1;
-            }else {
-                feli = feli + 1;
+            for (int i = 0; i < n; i++) {
+                array[i] = reader.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (array[i] == i + 1) {
+                    infeli++;
+                }
+            }
+
+            if (infeli == 0) {
+                System.out.println(0);
+            } else {
+                System.out.println(1 + (infeli - 1) / 2);
             }
         }
 
-            System.out.println(infeli - 1);
-
-
+        reader.close();
     }
-
-
-
-
-
 }
